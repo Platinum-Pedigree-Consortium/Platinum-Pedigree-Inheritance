fa7d62ff56ac3db994c6bf4dcc5c66e1066b547c
Daniel Baker <dbaker@pacificbiosciences.com>
2024-02-13 10:22:07 -0800

    Errors + doctest for line parsing.

--- code/rust/src/bin/gfa2bcf.rs
+++ code/rust/src/bin/gfa2bcf.rs
@@ -1,28 +1,34 @@
-use clap::Parser;
-use log::info;
-use std::collections::HashSet;
-use std::fmt;
-use std::fs::read_to_string;
-use std::fs::File;
-use std::io::Write;
-use std::path::PathBuf;
+use concordance::gfa;
 
+use clap::Parser;
 use log::LevelFilter;
+use rust_htslib::bcf::{self, Read};
+
+use std::boxed::Box;
+use std::path::PathBuf;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
+    #[clap(help = "Path to input gfa")]
     gfa: PathBuf,
-    #[arg(short, long, help = "Input vcf path")]
+    #[arg(help = "Input {b,v}cf path")]
     vcf: PathBuf,
     #[arg(short, long, help = "Output bcf path")]
-    bcf: PathBuf,
+    bcf: Option<PathBuf>,
     #[arg(short = 'v', long = "verbose", action = clap::ArgAction::Count, default_value_t = 0)]
     verbosity: u8,
+    #[arg(
+        short,
+        long,
+        help = "Emit uncompressed output",
+        default_value_t = false
+    )]
+    uncompressed: bool,
 }
 
+/*
 #[derive(Debug)]
 struct VcfRecord {
     chrom: String,
@@ -55,11 +61,135 @@ impl fmt::Display for VcfRecord {
         )
     }
 }
+*/
 
-fn main() {
-    let mut failed_sites: i64 = 0;
-    let mut passed_sites: i64 = 0;
+#[derive(Debug)]
+struct VcfFields {
+    // "END=373973;AN=7;NS=7;NA=2;ALEN=74,0;AC=2;VS=>s24;VE=>s25;AWALK=>s67628,*"
+    pub end: Option<i32>,
+    pub alen: Vec<i32>,
+    pub allele_walk: Vec<String>,
+    vertex_start: String,
+    vertex_end: String,
+}
 
+impl VcfFields {
+    pub fn new(record: &bcf::Record) -> Result<Self, Box<dyn std::error::Error>> {
+        let end = record.info(b"END"); //("END tag missing");
+        let alen = record.info(b"ALEN"); //("ALEN tag missing");
+        let awalk = record.info(b"AWALK"); //("AWALK tag missing");
+        let vertex_start = record.info(b"VS"); //("VS tag missing");
+        let vertex_end = record.info(b"VE"); //("VE tag missing");
+
+        let alen = alen.integer()?.map(|x| x.to_owned()).unwrap_or_default();
+        let end = end.integer()?.and_then(|end| end.get(0).copied());
+        let awalk = awalk.string()?;
+        let allele_walk = awalk
+            .map(|awalk| {
+                awalk
+                    .iter()
+                    .map(|awalk| {
+                        std::str::from_utf8(&awalk[..])
+                            .expect("walk piece was not utf-8")
+                            .to_string()
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .unwrap_or_default();
+        let vertex_start =
+            std::str::from_utf8(vertex_start.string()?.expect("VS was not present")[0])?.to_owned();
+        let vertex_end =
+            std::str::from_utf8(vertex_end.string()?.expect("VE was not present")[0])?.to_owned();
+        Ok(Self {
+            end,
+            alen,
+            allele_walk,
+            vertex_start,
+            vertex_end,
+        })
+    }
+
+    /// Get id for vertex for start.
+    /// Trims the leading '>' character.
+    /// If no '>' is present, uses the string directly.
+    pub fn vertex_start(&self) -> &str {
+        let start = usize::from(self.vertex_start.starts_with('>'));
+        &self.vertex_start[start..]
+    }
+
+    /// Get id for vertex for end.
+    /// Trims the leading '>' character.
+    /// If no '>' is present, uses the string directly.
+    pub fn vertex_end(&self) -> &str {
+        let start = usize::from(self.vertex_end.starts_with('>'));
+        &self.vertex_end[start..]
+    }
+}
+
+fn core(args: &Args) -> Result<(), Box<dyn std::error::Error>> {
+    log::info!("Loading gfa: {:?}.", args.gfa);
+    let gfa = gfa::File::from_path(&args.gfa)?;
+
+    /*
+    if log::log_enabled!(log::Level::Trace) {
+        for (segment_id, segment) in gfa.segments.iter().enumerate() {
+            let range = gfa.out_edges(segment_id);
+        }
+    }
+    */
+
+    log::info!("Loaded gfa: {:?}.", args.gfa);
+    log::info!("Opening vcf: {:?}.", args.vcf);
+    let mut reader = bcf::Reader::from_path(&args.vcf)?;
+    let header = bcf::Header::from_template(reader.header()); // TODO: add new header info potentially. Maybe auto-handle missing vcf fields.
+
+    let bcf = args
+        .bcf
+        .as_ref()
+        .map_or("-".to_string(), |x| x.display().to_string());
+    let mut writer = bcf::Writer::from_path(&bcf, &header, args.uncompressed, bcf::Format::Bcf)?;
+    log::info!("Opened handles; reading in data.");
+    let mut all_records = Vec::new();
+    //let mut reader = bcf::Reader::from_path(&args.vcf)?;
+    for record in reader.records() {
+        let mut record = record?;
+        writer.translate(&mut record);
+        record.unpack();
+        let record_data = VcfFields::new(&record)?;
+        all_records.push((record, record_data));
+    }
+    log::info!("Read in data, processing");
+    for (record, data) in &mut all_records {
+        let vertex_start_str = data.vertex_start();
+        log::trace!("Getting vertex start: {vertex_start_str}");
+        let vertex_start = gfa.segment_id(&vertex_start_str).unwrap_or_else(|| {
+            panic!(
+                "No vertex found for data {data:?} with vs {vertex_start_str}/{:?}",
+                data.vertex_start
+            )
+        });
+        let vertex_end_str = data.vertex_end();
+        log::trace!("Getting vertex end: {vertex_end_str}");
+        let vertex_end = gfa.segment_id(&vertex_end_str).unwrap_or_else(|| {
+            panic!(
+                "No vertex found for data {data:?} with ve {:?}",
+                data.vertex_end
+            )
+        });
+        log::trace!("VE,VS: {vertex_start}, {vertex_end} ({vertex_start_str}, {vertex_end_str})");
+    }
+    const _TRUE_INSERTION: (i32, i32) = (0, 148_288_482 - 1); //chr1    148288482
+    const _TRUE_DELETION: (i32, i32) = (0, 150_411_410 - 1); // chr1    150411410
+
+    log::info!("Processed, writing out");
+    for (record, _data) in &all_records {
+        writer.write(record)?;
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     let filter_level: LevelFilter = match args.verbosity {
@@ -72,4 +202,8 @@ fn main() {
         .format_timestamp_millis()
         .filter_level(filter_level)
         .init();
+
+    core(&args)?;
+
+    Ok(())
 }
--- code/rust/src/gfa.rs
+++ code/rust/src/gfa.rs
@@ -1,8 +1,32 @@
 use std::boxed::Box;
+use std::collections::{BTreeMap, HashMap};
+use std::path::Path;
 use std::str::FromStr;
 
+#[repr(u8)]
+#[derive(Clone, Copy)]
 pub enum Category {
     Segment,
+    Link,
+}
+
+impl std::default::Default for Category {
+    fn default() -> Self {
+        Self::Segment
+    }
+}
+
+impl std::fmt::Display for Category {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Segment => "S",
+                Self::Link => "L",
+            }
+        )
+    }
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -21,16 +45,75 @@ impl std::str::FromStr for Category {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
             "S" => Ok(Category::Segment),
+            "L" => Ok(Category::Link),
             _ => Err(Error(format!("Failed to get category: {s}"))),
         }
     }
 }
 
+///
+///
+///```
+/// use concordance::gfa::Line;
+/// use std::str::FromStr;
+/// let line = Line::from_str("S\tS1\tACGT\tAC:Z:0").expect("Failed to parse line");
+/// eprintln!("Line: {line}");
+/// assert_eq!(line.to_string(), "S\tS1\tACGT\tAC:Z:0");
+/// assert_eq!(line.name(), "S1");
+///```
+#[derive(Clone, Default)]
 pub struct Line {
     pub category: Category,
-    pub name: String,
-    pub sequence: Vec<u8>,
+    pub name: Option<String>,
+    pub sequence: Option<String>,
     pub tags: Vec<String>,
+    pub from: Option<String>,
+    pub to: Option<String>,
+    pub cigar: Option<String>,
+    pub from_orientation: bool,
+    pub to_orientation: bool,
+}
+
+impl Line {
+    #[must_use]
+    pub fn name(&self) -> String {
+        if let Some(name) = &self.name {
+            name.clone()
+        } else {
+            format!("{:?}->{:?}", self.from().unwrap(), self.to().unwrap())
+        }
+    }
+
+    #[must_use]
+    fn from(&self) -> Option<&String> {
+        self.from.as_ref()
+    }
+
+    #[must_use]
+    fn to(&self) -> Option<&String> {
+        self.to.as_ref()
+    }
+}
+
+/// Store orientation as a bool. True = forward strand.
+#[must_use]
+fn orientation(s: &str) -> bool {
+    match s {
+        "+" => true,
+        "-" => false,
+        _ => {
+            panic!("Invalid orientation {s}");
+        }
+    }
+}
+
+#[must_use]
+fn fmtstrand(x: bool) -> char {
+    if x {
+        '+'
+    } else {
+        '-'
+    }
 }
 
 impl std::str::FromStr for Line {
@@ -41,38 +124,227 @@ impl std::str::FromStr for Line {
             Category::from_str(toks.next().ok_or_else(|| {
                 Error(format!("Expected token type in malformatted string {s}"))
             })?)?;
-        let name = toks
-            .next()
-            .ok_or_else(|| Error(format!("Expected name type in malformatted string {s}")))?
-            .to_owned();
-        let sequence = toks
-            .next()
-            .ok_or_else(|| Error(format!("Expected seq type in malformatted string {s}")))?
-            .as_bytes()
-            .to_owned();
-        let tags = toks.map(|x| x.to_string()).collect::<Vec<_>>();
-        Ok(Self {
-            category,
-            name,
-            sequence,
-            tags,
-        })
+        match category {
+            Category::Segment => {
+                let name = Some(
+                    toks.next()
+                        .ok_or_else(|| {
+                            Error(format!("Expected name type in malformatted string {s}"))
+                        })?
+                        .to_owned(),
+                );
+                let sequence = Some(
+                    toks.next()
+                        .ok_or_else(|| {
+                            Error(format!("Expected seq type in malformatted string {s}"))
+                        })?
+                        .to_owned(),
+                );
+                let tags = toks
+                    .map(std::string::ToString::to_string)
+                    .collect::<Vec<_>>();
+                Ok(Self {
+                    category,
+                    name,
+                    sequence,
+                    tags,
+                    ..Default::default()
+                })
+            }
+            Category::Link => {
+                let from = Some(
+                    toks.next()
+                        .ok_or_else(|| Error(format!("Expected from segment id in s {s}")))
+                        .map(|x| x.to_owned())?,
+                );
+                let from_orientation = orientation(
+                    toks.next()
+                        .ok_or_else(|| Error("Expected from orientation".to_string()))?,
+                );
+                let to = Some(
+                    toks.next()
+                        .ok_or_else(|| Error(format!("Expected to segment id in s {s}")))
+                        .map(|x| x.to_owned())?,
+                );
+                let to_orientation = orientation(
+                    toks.next()
+                        .ok_or_else(|| Error("Expected to orientation".to_string()))?,
+                );
+                let cigar = Some(
+                    toks.next()
+                        .map(|x| x.to_owned())
+                        .ok_or_else(|| Error("Expected cigar".into()))?,
+                );
+                let tags = toks
+                    .map(std::string::ToString::to_string)
+                    .collect::<Vec<_>>();
+                Ok(Self {
+                    category,
+                    tags,
+                    cigar,
+                    from,
+                    to,
+                    from_orientation,
+                    to_orientation,
+                    ..Default::default()
+                })
+            }
+        }
     }
 }
 
+impl std::fmt::Display for Line {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.category {
+            Category::Segment => {
+                let res = write!(
+                    f,
+                    "{}\t{}\t{}",
+                    self.category,
+                    self.name.as_ref().unwrap(),
+                    self.sequence.as_ref().unwrap()
+                );
+                res.and_then(|x| {
+                    if self.tags.is_empty() {
+                        Ok(x)
+                    } else {
+                        write!(
+                            f,
+                            "\t{}",
+                            itertools::Itertools::intersperse(
+                                self.tags.iter().map(|x| &x[..]),
+                                "\t"
+                            )
+                            .collect::<String>()
+                        )
+                    }
+                })
+            }
+            Category::Link => {
+                let res = write!(
+                    f,
+                    "{}\t{}\t{}\t{}\t{}\t{}",
+                    self.category,
+                    self.from().unwrap(),
+                    fmtstrand(self.from_orientation),
+                    self.to().unwrap(),
+                    fmtstrand(self.to_orientation),
+                    self.cigar.as_ref().unwrap()
+                );
+                res.and_then(|x| {
+                    if self.tags.is_empty() {
+                        Ok(x)
+                    } else {
+                        write!(
+                            f,
+                            "\t{}",
+                            itertools::Itertools::intersperse(
+                                self.tags.iter().map(|x| &x[..]),
+                                "\t"
+                            )
+                            .collect::<String>()
+                        )
+                    }
+                })
+            }
+        }
+    }
+}
+
+/// Holds segments + links + edges.
+/// Look up the numeric id for a segment with `name2idx`.
+/// Find excident edges in `edges` using a `BTreeMap` range query.
 pub struct File {
-    pub lines: Vec<Line>,
+    pub segments: Vec<Line>,
+    pub links: Vec<Line>,
+    pub edges: BTreeMap<(usize, usize), usize>,
+    pub name2idx: HashMap<String, usize>,
 }
 
 impl File {
+    /// Build `gfa::File` from a type implementing `std::io::Read`
+    ///
+    /// # Errors
+    /// 1. Lines not valid UTF-8.
+    /// 2. Lines are not valid gfa.
+    /// 3. Lines are gfa lines we don't support yet.
     pub fn from_reader(x: impl std::io::Read) -> Result<Self, Box<dyn std::error::Error>> {
         use std::io::BufRead;
         let reader = std::io::BufReader::new(x);
-        let mut lines = Vec::with_capacity(8192);
+        let mut segments = Vec::with_capacity(8192);
+        let mut links = Vec::with_capacity(8192);
+        let mut edges = BTreeMap::<(usize, usize), usize>::new();
+        let mut name2idx = HashMap::new();
+        let mut line_num = 0usize;
         for input in reader.lines() {
+            line_num += 1;
+            if line_num % 10000 == 0 {
+                log::info!("Parsed {line_num} lines from gfa file");
+            }
             let input = input?;
-            lines.push(Line::from_str(&input)?);
+            let line = Line::from_str(&input)?;
+            match line.category {
+                Category::Link => {
+                    let to = line.to().and_then(|x| name2idx.get(x)).expect("Missing to");
+                    let from = line
+                        .from()
+                        .and_then(|x| name2idx.get(x))
+                        .expect("Missing from");
+                    edges.insert((*to, *from), links.len());
+                    links.push(line);
+                }
+                Category::Segment => {
+                    name2idx.insert(line.name.as_ref().unwrap().clone(), segments.len());
+                    segments.push(line);
+                }
+            }
         }
-        Ok(Self { lines })
+        Ok(Self {
+            segments,
+            links,
+            edges,
+            name2idx,
+        })
+    }
+
+    /// Build `gfa::File` from a `Path`.
+    ///
+    /// # Errors
+    /// 1. File does not exist.
+    /// 2. Cannot read from file.
+    /// 3. Lines not valid UTF-8.
+    /// 4. Lines are not valid gfa.
+    /// 5. Lines are gfa lines we don't support yet.
+    pub fn from_path(x: impl AsRef<Path>) -> Result<Self, Box<dyn std::error::Error>> {
+        let path = std::fs::File::open(x.as_ref())?;
+        Self::from_reader(path)
+    }
+
+    /// Returns all excident edges for a given segment id.
+    /// # Panics
+    /// Panics if start > end, which can never happen in software as `0 < usize::MAX`.
+    pub fn out_edges(
+        &self,
+        idx: usize,
+    ) -> std::collections::btree_map::Range<'_, (usize, usize), usize> {
+        self.edges.range((
+            std::ops::Bound::Included(&(idx, 0)),
+            std::ops::Bound::Included(&(idx, usize::MAX)),
+        ))
+    }
+
+    pub fn segment_id(&self, name: impl AsRef<str>) -> Option<usize> {
+        self.name2idx.get(name.as_ref()).copied()
+    }
+}
+
+impl std::fmt::Display for File {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "GFAFile{{{} segments, {} links}}",
+            self.segments.len(),
+            self.links.len()
+        )
     }
 }
