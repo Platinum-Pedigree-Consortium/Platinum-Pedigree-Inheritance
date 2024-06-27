4b519a6c611f6a523fd06236f802b2855020103b
egor-dolzhenko <egor.dolzhenko@gmail.com>
2024-06-27 11:45:29 -0700

    Update analysis

--- pipelines/tandem-repeats/helpers/consistency.py
+++ pipelines/tandem-repeats/helpers/consistency.py
@@ -38,7 +38,7 @@ def load_ivecs(path):
     return vecs
 
 
-def get_candidates(ivec, tr_by_sample):
+def get_candidates(ivec, alleles_by_sample):
     r"""Get candidates for each haplotype
 
     Given a haplotype (A, B, C, or D), we extract the set of all alleles
@@ -50,7 +50,7 @@ def get_candidates(ivec, tr_by_sample):
     ----------
     ivec : dict
         Inheritance vector containing haplotype assignment for each sample
-    tr_by_sample : dict
+    alleles_by_sample : dict
         Allele sequences for each sample
 
     Returns
@@ -80,20 +80,20 @@ def get_candidates(ivec, tr_by_sample):
     for hap, samples in get_hap_to_samples(ivec).items():
         hap_alleles = []
         for sample in samples:
-            hap_alleles.extend(tr_by_sample[sample].alleles)
+            hap_alleles.extend([al.seq for al in alleles_by_sample[sample]])
         candidates = get_frequent_seqs(hap_alleles, num_top=2)
         hap_to_candidates[hap] = candidates
     return hap_to_candidates
 
 
-def eval_assignment(ivec, tr_by_sample, assign):
+def eval_assignment(ivec, alleles_by_sample, assign):
     r"""Evalute an assignment of alleles to haplotypes
 
     Parameters
     ----------
     ivec : dict
         Inheritance vector
-    tr_by_sample : dict
+    alleles_by_sample : dict
         A mapping from sample ids to alleles
     assign : Assign
         An assignment of alleles to haplotypes
@@ -123,7 +123,7 @@ def eval_assignment(ivec, tr_by_sample, assign):
             continue
 
         expected = tuple(getattr(assign, hap) for hap in haps)
-        observed = tr_by_sample[sample].alleles
+        observed = [a.seq for a in alleles_by_sample[sample]]
         assert len(expected) == len(observed)
 
         if len(expected) == 2:
@@ -147,14 +147,14 @@ def eval_assignment(ivec, tr_by_sample, assign):
     return hap_to_dists
 
 
-def get_best_assign(ivec, tr_by_sample, allele_candidates_by_hap):
+def get_best_assign(ivec, alleles_by_sample, allele_candidates_by_hap):
     r"""Pick a top-scoring assignment of alleles to haplotypes
 
     Parameters
     ----------
     ivec : dict
         Inheritance vector
-    tr_by_sample : dict
+    alleles_by_sample : dict
         A mapping between sample ids and allele sequences
     allele_candidates_by_hap : dict
         A mapping between haplotypes and alleles that may contain
@@ -176,7 +176,7 @@ def get_best_assign(ivec, tr_by_sample, allele_candidates_by_hap):
     assign_to_dists = []
     for rec in itertools.product(*allele_candidates):
         assign = Assign(*rec)
-        hap_to_dists = eval_assignment(ivec, tr_by_sample, assign)
+        hap_to_dists = eval_assignment(ivec, alleles_by_sample, assign)
         assign_to_dists.append((assign, hap_to_dists))
 
     assign_to_dists.sort(key=get_score)
